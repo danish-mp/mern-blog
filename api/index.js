bfe1e15cee9dc567eb841f1165582934b9c3dc20
danish-mp <danishkundai@gmail.com>
2024-07-31 15:57:23 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -15,6 +15,8 @@ mongoose
     console.log(error.message);
   });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!!");
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
