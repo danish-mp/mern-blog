07842e509fdc99b6fdda6ae66656f22114f36bc4
danish-mp <danishkundai@gmail.com>
2024-07-31 22:22:26 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -27,3 +30,4 @@ app.listen(PORT, () => {
 // });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
