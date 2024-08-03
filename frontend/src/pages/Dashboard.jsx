5907fcdc2e3c202345f9343cdbc4e803880472ae
danish-mp <danishkundai@gmail.com>
2024-08-03 22:51:06 +0530

    complete sidebar of the dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 function Dashboard() {
+  const [tab, setTab] = useState("");
+  const location = useLocation();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
-    <div>
-      Dashboard
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+
+      {/* profile... */}
+      {tab === "profile" && <DashProfile />}
+
+      {/* posts... */}
+      {/* {tab === "posts" && <DashPosts />} */}
+
+      {/* users */}
+      {/* {tab === "users" && <DashUsers />} */}
+
+      {/* comments  */}
+      {/* {tab === "comments" && <DashComments />} */}
+
+      {/* dashboard comp */}
+      {/* {tab === "dash" && <DashboardComp />} */}
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
