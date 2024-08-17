01f9c07db7fcffc23162505edc9f415328f82f54
danish-mp <danishkundai@gmail.com>
2024-08-17 17:43:24 +0530

    made changes on homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -66,7 +66,7 @@ export default function Home() {
       <div className="max-w-6xl mx-auto p-3 py-7 flex justify-evenly">
         {skeleton ? (
           <div className="flex flex-col gap-6">
-            <div className="flex flex-wrap gap-4">
+            <div className="flex flex-wrap gap-4 ml-8">
               {num.map((item, index) => (
                 <CardSkeleton key={index} />
               ))}
